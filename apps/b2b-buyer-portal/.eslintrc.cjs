b9917ba28f132c6b59358177ae3bffcae514cd75
Matthew Volk <matt.volk@bigcommerce.com>
2025-09-03 14:52:29 -0500

    chore(b2b): merge eslint-config-b2b

--- apps/b2b-buyer-portal/.eslintrc.cjs
+++ apps/b2b-buyer-portal/.eslintrc.cjs
@@ -1,6 +1,63 @@
 module.exports = {
-  "extends": ["b2b"],
+  root: true,
+  ignorePatterns: ['dist/', 'out/', 'build/', 'coverage/'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['react', '@typescript-eslint', 'testing-library', 'simple-import-sort'],
+  reportUnusedDisableDirectives: true,
+  "extends": [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    '@bigcommerce/eslint-config',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: process.cwd(),
+  },
   "rules": {
+    'prettier/prettier': ['warn'],
+    'react/react-in-jsx-scope': 0,
+    'react/require-default-props': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/tests/**/*.ts',
+          '**/tests/**/*.tsx',
+          '**/*.test.ts',
+          '**/*.test.tsx',
+          '**/__mocks__/**',
+          'vite.config.ts',
+        ],
+      },
+    ],
+    'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
+    'import/prefer-default-export': 'off',
+    'no-implicit-coercion': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-useless-template-literals': 'error',
     "no-restricted-imports": [
       "error",
       {
@@ -20,6 +77,38 @@ module.exports = {
     ]
   },
   "overrides": [
+    {
+      files: ['**/slices/*.ts'],
+      rules: { 'no-param-reassign': ['error', { props: false }] },
+    },
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
+          },
+        ],
+      },
+    },
     {
       "files": ["src/**/*.test.{ts,tsx}", "src/**/*.spec.{ts,tsx}"],
       "rules": {
